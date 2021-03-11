72fec1f60ca9abc6f24242ee133a6608430a0f57
invisibleyoubic <65252697+invisibleyoubic@users.noreply.github.com>
2021-03-11 11:59:44 +0300

    Список
    Задание, где нужно создать свой список

---
+++ MyList.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace list
+{
+    class MyList<T> : IEnumerable
+    {
+        static int size = 0;
+        static T[] array = new T[size];
+
+        public IEnumerator GetEnumerator()
+        {
+            for(int i = 0; i < size; i++)
+                yield return array[i];
+        }
+
+        public void Add(T item)
+        {
+            Array.Resize<T>(ref array, ++size);
+            array[size - 1] = item;
+        }
+
+        public void Insert(int index, T item)
+        {
+            if(index > size)
+            {
+                size += index;
+                Array.Resize<T>(ref array, size);
+                array[index] = item;
+            }
+            if(index < 0)
+            {
+                throw new Exception("Index < 0");
+            }
+            if ((index >= 0) && (index < size))
+            {
+                array[index] = item;
+            }
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index > size)
+            {
+                throw new Exception("Index > Size");
+            }
+            if (index > 0)
+            {
+                throw new Exception("Index < 0");
+            }
+            if ((index >= 0) && (index < size))
+            {
+                for(int i = index; i < size - 1; i++)
+                {
+                    array[i] = array[i + 1];
+                }
+                Array.Resize<T>(ref array, --size);
+            }
+        }
+
+        public T Last()
+        {
+            return array[size-1];
+        }
+
+        public T First()
+        {
+            return array[0];
+        }
+
+        public void Clear()
+        {
+            Array.Resize<T>(ref array, 0);
+        }
+
+        public int Size()
+        {
+            return size;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,137 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
-namespace Stack
+
+namespace list
 {
-    //class Program
-    //{
-    //    static void Main(string[] args)
-    //    {
-    //        MyStack<string> mystack = new MyStack<string>();
-
-    //        System.Console.WriteLine(mystack.Size());
-    //        System.Console.WriteLine(mystack.Peek());
-
-    //        mystack.Push("first");
-    //        mystack.Push("second");
-
-    //        System.Console.WriteLine(mystack.Empty());
-    //        System.Console.WriteLine(mystack.Size());
-
-    //        mystack.Push("third");
-    //        mystack.Push("fourth");
-
-    //        while (!mystack.Empty())
-    //        {
-    //            System.Console.WriteLine(mystack.Peek());
-    //            mystack.Pop();
-    //        }
-
-    //        mystack.Pop();
-    //        System.Console.WriteLine(mystack.Empty());
-
-    //    }
-    //}
-
     class Program
     {
-        static int[,] array;
-        static int n = 0;
-        static MyStack<KeyValuePair<int, int>> mystack = new MyStack<KeyValuePair<int, int>>();
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
+            MyList<int> list = new MyList<int>();
 
-            Console.WriteLine("Ввести размер массива: ");
-            n = Convert.ToInt32(Console.ReadLine());
-            array = new int[n, n];
-            Fill();
-            Print();
-            toString(Count());
-            mystack.Push(new KeyValuePair<int, int>(3, 2));
-            while (!mystack.Empty())
-            {
-                FloodFill(mystack.Pop(), 2, 0);
-            }
+            list.Add(1);
+            Console.WriteLine(list.First());
+            Console.WriteLine(list.Last());
+            list.Insert(5, 9);
+            Console.WriteLine($"Size = {list.Size()}");
+            Console.WriteLine(list.Last());
+            list.Insert(0, 6);
+
             Console.WriteLine();
-            Print();
-            toString(Count());
-            Console.WriteLine("Success");
-            Console.ReadKey();
-        }
-
-        public static void Fill()  //Заполнение
-        {
-            Random r = new Random(1);
-            for (int i = 0; i < n; i++)
+            foreach (var item in list)
             {
-                for (int j = 0; j < n; j++)
-                {
-                    if (Convert.ToInt32(r.Next(0, 4)) == 3)
-                    {
-                        array[i, j] = 1;
-                    }
-                    else
-                    {
-                        array[i, j] = 0;
-                    }
-                }
+                Console.WriteLine(item);
             }
-        }
+            list.RemoveAt(0);
 
-        public static void Print()     //Печать
-        {
-            for (int i = 0; i < n; i++)
+            Console.WriteLine();
+            foreach (var item in list)
             {
-                for (int j = 0; j < n; j++)
-                {
-                    Console.Write(array[i, j]);
-                }
-                Console.WriteLine();
-            }
-        }
-
-        public static void FloodFill(KeyValuePair<int, int> pair, int color, int point)  //Закраска
-        {
-            if ((pair.Key >= 0) && (pair.Value >= 0) && (pair.Key < n) && (pair.Value < n) && array[pair.Key, pair.Value] == point)
-            {
-                array[pair.Key, pair.Value] = color;
-                mystack.Push(new KeyValuePair<int, int>(pair.Key + 1, pair.Value));
-                mystack.Push(new KeyValuePair<int, int>(pair.Key - 1, pair.Value));
-                mystack.Push(new KeyValuePair<int, int>(pair.Key, pair.Value + 1));
-                mystack.Push(new KeyValuePair<int, int>(pair.Key, pair.Value - 1));
-            }
-        }
-
-        public static int[] Count()   //Счетчик
-        {
-            int[] counts = new int[3];
-
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j < n; j++)
-                {
-                    if (array[i, j] == 0)
-                    {
-                        counts[0]++;
-                    }
-                    if (array[i, j] == 1)
-                    {
-                        counts[1]++;
-                    }
-                    if (array[i, j] == 2)
-                    {
-                        counts[2]++;
-                    }
-                }
-            }
-
-            return counts;
-        }
-
-        public static void toString(int[] array)   //Напечатать счетчик
-        {
-            for (int i = 0; i < array.Length; i++)
-            {
-                Console.Write($"{array[i]} ");
+                Console.WriteLine(item);
             }
         }
     }
